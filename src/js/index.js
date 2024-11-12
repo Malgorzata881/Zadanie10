6b8178c16c8ee1737f5df4867653421ebf0e12ec
Malgorzata881 <m.michalczuk7@o2.pl>
2024-11-12 19:56:43 +0100

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import './styles.css';
+// import './styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
